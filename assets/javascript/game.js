3d065a1a0d7ba861bbedaf3f41414c42220678ca
Yaraye <yaraye@gmail.com>
2018-06-06 00:53:17 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,124 @@
+
+//assign global variable 
+var wins=0;
+var losses=0;
+var totalScore=0;
+var score=0;
+var playersGuess="";
+//assign to html
+$("#wins").html(wins);
+$('#losses').html(losses);
+$('#score').html(score);
+$('#userTotal1').html(userTotal);
+
+
+//The random number shown at the start of the game should be between 19 - 120.
+
+    var randomeNumber = Math.floor(Math.random() * 101) + 19;
+    console.log (randomeNumber);
+    $('#randomNumber').html(randomeNumber);
+
 //The player will have to guess the answer. This time, though, the player will guess with numbers instead of letters. 
+//Each crystal should have a random hidden value between 1 - 12.
+    var cry1 = Math.floor(Math.random() * 11) + 1;
+    var cry2 = Math.floor(Math.random() * 11) + 1;
+    var cry3 = Math.floor(Math.random() * 11) + 1;
+    var cry4 = Math.floor(Math.random() * 11) + 1;
 
+    function reset()
+    {
+        var randomeNumber = Math.floor(Math.random() * 101) + 19;
+        console.log (randomeNumber);
+        $('#randomNumber').html(randomeNumber);
+    
+    //The player will have to guess the answer. This time, though, the player will guess with numbers instead of letters. 
+    //Each crystal should have a random hidden value between 1 - 12.
+        var cry1 = Math.floor(Math.random() * 11) + 1;
+        var cry2 = Math.floor(Math.random() * 11) + 1;
+        var cry3 = Math.floor(Math.random() * 11) + 1;
+        var cry4 = Math.floor(Math.random() * 11) + 1;
+        userTotal=0;
+    
+    }
+    
 //There will be four crystals displayed as buttons on the page.
+$('#red').on ('click', function(){
+    userTotal = userTotal + cry1;
+    console.log("New userTotal= " + userTotal);
+    $('#score').html(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == randomeNumber){
+           
+            wins++; 
+            $('#wins').html(wins);
+            reset();
+        }
+        else if ( userTotal > randomeNumber){
+            losses++; 
+            $('#losses').html(losses);
+            reset();
+        }   
+  })  
+  $('#blue').on ('click', function(){
+    userTotal = userTotal + cry2;
+    console.log("New userTotal= " + userTotal);
+    $('#score').html(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == randomeNumber){
+           
+            wins++; 
+            $('#wins').html(wins);
+            reset();
+        }
+        else if ( userTotal > randomeNumber){
+            losses++; 
+            $('#losses').html(losses);
+            reset();
+        }   
+  })  
+  $('#yellow').on ('click', function(){
+    userTotal = userTotal + cry3;
+    console.log("New userTotal= " + userTotal);
+    $('#score').html(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == randomeNumber){
+          
+          wins++; 
+        $('#wins').html(wins);
+        reset();
 
-//The player will be shown a random number at the start of the game.
+        }
+        else if ( userTotal > randomeNumber){
+         
+          losses++; 
+          $('#losses').html(losses);
+          reset();
+  
+        }   
+  })
+  $('#green').on ('click', function(){
+    userTotal = userTotal + cry4;
+    console.log("New userTotal= " + userTotal);
+    $('#score').html(userTotal); 
+          //sets win/lose conditions
+        if (userTotal == randomeNumber){
+           
+            wins++; 
+            $('#wins').html(wins);
+            reset();
+        }
+        else if ( userTotal > randomeNumber){
+            losses++; 
+            $('#losses').html(losses);
+            reset();
+        }   
+  })    
 
+
+    
 //When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
+userTotal = score + cry1;
+console.log(userTotal);
 
 //Your game will hide this amount until the player clicks a crystal.
 //When they do click one, update the player's score counter.
